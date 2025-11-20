40ea7d8ff927084726af03c917857a3fa47f55ca
kuroi <kuroi@gmail.com>
2025-11-20 14:45:33 +0800

    fix

--- goto.rb
+++ goto.rb
@@ -5,6 +5,7 @@ class Goto < Formula
   sha256 "ede0f7d5cf52d170e8804e825e7c8b8d5ae1f4a79815a65effb351fea330010d"
 
   def install
+    bin.install "goto"
     # 给执行权限（保险起见）
     chmod 0755, bin/"goto"
   end
