af524970723333fad7503cd7f922f8ed86e2cc58
kuroi <kuroi@gmail.com>
2025-12-22 15:38:47 +0800

    update

--- goto.rb
+++ goto.rb
@@ -1,8 +1,8 @@
 class Goto < Formula
   desc "ssh管理工具"
   homepage "https://github.com/kuroimachi/goto"
-  url "https://github.com/kuroimachi/goto/releases/download/v1.0.2/goto"
-  sha256 "e5fcf11fcc102da635d8dc8aaf7aa529861f10aeb6ad690f44c5912730622ece"
+  url "https://github.com/kuroimachi/goto/releases/download/v1.0.3/goto"
+  sha256 "7ca6fc5018d362dde8f83278c29e5e0bfec0c3f7b0e729739f4b797429eec9e5"
 
   def install
     bin.install "goto"
