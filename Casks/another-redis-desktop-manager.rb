4d932e76a37ec034acd682b1e3b452b07e4b3b9e
kuroi <kuroi@gmail.com>
2026-01-27 14:54:38 +0800

    v1.0.1

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,5 +1,5 @@
 cask "another-redis-desktop-manager" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "35f107d90e8cf249229ce5b0a4dcdffde2452f0034214c4c8bd88292dd846c88"
 
   url "https://github.com/kuroimachi/AnotherRedisDesktopManager/releases/download/v#{version}/Another.Redis.Desktop.Manager.zip"
